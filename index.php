3946370f3b9a930a65affbf30e54d940a0c3db02
Marcel <marcelk1809@gmail.com>
2017-02-06 16:40:05 +0100

    bestand inladen

--- index.php
+++ index.php
@@ -1,19 +1,39 @@
 <?php
+error_reporting(E_ALL);
 include_once ("php/include/class.TemplatePower.inc.php");
 include_once ("php/include/database.php");
 
 include("php/header.php");
 
-$getBestand = $db->prepare("SELECT * FROM bestand 
-WHERE bestandid = :bestand");
-$getBestand->bindParam(":bestand", $_GET['bestand']);
-$getBestand->execute();
+if(!empty($_GET['bestand'])){
+    $checkBestand = $db->prepare("SELECT count(*) FROM bestand 
+          WHERE bestandid = :bestand");
+    $checkBestand->bindParam(":bestand", $_GET['bestand']);
+    $checkBestand->execute();
 
-$bestand = $getBestand->fetch(PDO::FETCH_ASSOC);
+    if($checkBestand->fetchColumn() == 1){
+        // 1 rij gevonden met bestandid
+        $getBestand = $db->prepare("SELECT * FROM bestand 
+          WHERE bestandid = :bestand");
+        $getBestand->bindParam(":bestand", $_GET['bestand']);
+        $getBestand->execute();
 
-var_dump($bestand);
+        $bestand = $getBestand->fetch(PDO::FETCH_ASSOC);
 
+        if(file_exists("php/".$bestand['naam'])){
+            include("php/".$bestand['naam']);
+        }else{
+            print "bestand bestaat niet";
+        }
 
-include("php/content.php");
+        var_dump($bestand);
+    }else{
+        print "geen rij gevonden met dat id";
+
+    }
+}else{
+    print "Geen bestand in url";
+}
+
 include("php/aside.php");
 include("php/footer.php");
