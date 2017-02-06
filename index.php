276c0ddef302d1bfb2c347ef9c246a99c7be47a8
Marcel <marcelk1809@gmail.com>
2017-02-06 16:00:28 +0100

    eerste query

--- index.php
+++ index.php
@@ -1,7 +1,19 @@
 <?php
 include_once ("php/include/class.TemplatePower.inc.php");
+include_once ("php/include/database.php");
 
 include("php/header.php");
+
+$getBestand = $db->prepare("SELECT * FROM bestand 
+WHERE bestandid = :bestand");
+$getBestand->bindParam(":bestand", $_GET['bestand']);
+$getBestand->execute();
+
+$bestand = $getBestand->fetch(PDO::FETCH_ASSOC);
+
+var_dump($bestand);
+
+
 include("php/content.php");
 include("php/aside.php");
 include("php/footer.php");
---
+++ php/include/database.php
@@ -0,0 +1,5 @@
+<?php
+
+$db = new PDO('mysql:host=localhost;dbname=project','root','');
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
